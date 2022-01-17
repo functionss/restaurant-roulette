eff689b494c32b6eaa83dbdc55afb6d50bb420a8
jakedahn <jakedahn@gmail.com>
2022-01-16 17:44:42 -0700

    refactor: Pulling the SuggestionCard out into its own component, to clean up the home page

---
+++ components/SuggestionCard.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-html-link-for-pages */
+import React, { useState } from "react";
+
+import Image from "next/image";
+
+import PizzaIcon from "@icons/Pizza";
+import RefreshIcon from "@icons/Refresh";
+import AddIcon from "@icons/Add";
+
+import type { Suggestion } from "types";
+import { getSuggestion, createSuggestion } from "@fetchers/suggestions";
+import setupMirage from "@utils/mirage";
+
+import styles from "@styles/SuggestionCard.module.css";
+
+interface PropTypes {
+  suggestion: Suggestion;
+  loading: boolean;
+  same: boolean;
+}
+
+function SuggestionCard({ suggestion, loading, same }: PropTypes) {
+  const mapImage =
+    suggestion !== null
+      ? `https://maps.googleapis.com/maps/api/staticmap?markers=${suggestion.address}&center=${suggestion.name}+${suggestion.address}&zoom=16&size=640x300&scale=2&style=feature:poi.business|visibility:on&key=AIzaSyBxnrgh8XKoeQQ7McVIjhZNfOPNRaG0FOM`
+      : "";
+  const mapLink =
+    suggestion !== null
+      ? `https://www.google.com/maps/search/?api=1&query=${suggestion.name}+${suggestion.address}`
+      : "";
+
+  return (
+    <div className={`${styles.suggestion} ${loading ? styles.fade : ""}`}>
+      <div className={styles.title}>
+        <a href={suggestion.url} target="_blank" rel="noreferrer noopener">
+          <h2 className={styles.suggestionName}>{suggestion.name}</h2>
+        </a>
+        <h3 className={styles.suggestionAddress}>{suggestion.address}</h3>
+
+        {same && (
+          <p className={styles.same}>
+            Whoa, you spun the same restaurant twice in a row. it must be{" "}
+            <strong>DESTINY!</strong>
+          </p>
+        )}
+      </div>
+
+      <div className={styles.map}>
+        <a href={mapLink} target="_blank" rel="noreferrer noopener">
+          <Image
+            src={mapImage}
+            alt={`${suggestion.name} - ${suggestion.address}`}
+            width={640}
+            height={300}
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SuggestionCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import React, { useState } from "react";
 
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 import PizzaIcon from "@icons/Pizza";
 import RefreshIcon from "@icons/Refresh";
 import AddIcon from "@icons/Add";
 
 import type { Suggestion } from "types";
-import { getSuggestion, createSuggestion } from "@fetchers/suggestions";
+import { getSuggestion } from "@fetchers/suggestions";
 import setupMirage from "@utils/mirage";
 
 import styles from "@styles/Home.module.css";
 import NewSuggestionModalForm from "@components/NewSuggestionModalForm";
+import SuggestionCard from "@components/SuggestionCard";
 
 setupMirage();
 
@@ -32,16 +32,7 @@ const Home: NextPage = () => {
     setShowModal(true);
   };
 
-  const mapImage =
-    suggestion !== null
-      ? `https://maps.googleapis.com/maps/api/staticmap?markers=${suggestion.address}&center=${suggestion.name}+${suggestion.address}&zoom=16&size=640x300&scale=2&style=feature:poi.business|visibility:on&key=AIzaSyBxnrgh8XKoeQQ7McVIjhZNfOPNRaG0FOM`
-      : "";
-  const mapLink =
-    suggestion !== null
-      ? `https://www.google.com/maps/search/?api=1&query=${suggestion.name}+${suggestion.address}`
-      : "";
-
-  const clickHandler = async (e: React.SyntheticEvent) => {
+  const triggerSpin = async (e: React.SyntheticEvent) => {
     e.preventDefault();
 
     //  Start spinning as soon as the button is clicked
@@ -69,18 +60,17 @@ const Home: NextPage = () => {
 
       <div className={styles.wrapper}>
         <div className={styles.header}>
-          <a href="#" onClick={clickHandler}>
+          <a className={styles.brand} href="#" onClick={triggerSpin}>
             <PizzaIcon
               className={`${styles.pizzaLogo} ${loading ? styles.spin : ""}`}
               width={75}
               height={75}
             />
-          </a>
-          <a href="/">
             <h1>Restaurant Roulette</h1>
           </a>
+
           <div className={styles.actions}>
-            <button className="primary" onClick={clickHandler}>
+            <button className="primary" onClick={triggerSpin}>
               <RefreshIcon className="add-margin" /> Get Suggestion
             </button>
             <button className="secondary" onClick={openModal}>
@@ -100,41 +90,13 @@ const Home: NextPage = () => {
               {successMessage}
             </div>
           )}
+
           {suggestion && (
-            <div
-              className={`${styles.suggestion} ${loading ? styles.fade : ""}`}
-            >
-              <div className={styles.title}>
-                <a
-                  href={suggestion.url}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <h2 className={styles.suggestionName}>{suggestion.name}</h2>
-                </a>
-                <h3 className={styles.suggestionAddress}>
-                  {suggestion.address}
-                </h3>
-
-                {same && (
-                  <p className={styles.same}>
-                    Whoa, you spun the same restaurant twice in a row. it must
-                    be <strong>DESTINY!</strong>
-                  </p>
-                )}
-              </div>
-
-              <div className={styles.map}>
-                <a href={mapLink} target="_blank" rel="noreferrer noopener">
-                  <Image
-                    src={mapImage}
-                    alt={`${suggestion.name} - ${suggestion.address}`}
-                    width={640}
-                    height={300}
-                  />
-                </a>
-              </div>
-            </div>
+            <SuggestionCard
+              suggestion={suggestion}
+              same={same}
+              loading={loading}
+            />
           )}
         </div>
       </div>
